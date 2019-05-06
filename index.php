6183006276600f7c4344268e0f2a18aaf21f0cdb
PoojaBhosale101 <40861201+PoojaBhosale101@users.noreply.github.com>
2019-05-06 11:04:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$text = "hi shruti,Good Morning";
+$text = "hellow world,this is my first alexa skill!!!";
 $array = array("version"=>"1.0", "response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
 echo json_encode($array);
 ?>
